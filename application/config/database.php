f61e053dfc3a6ed12c5a4f04ccc8db2089671dc1
tiararahmania05 <tiararahmania05@gmail.com>
2021-03-21 09:30:24 +0700

    make it better

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db4free.net:3306',
+	'hostname' => 'db4free.net',
 	'username' => 'nelayanku2021',
 	'password' => 'Nelayanku@2021',
 	'database' => 'nelayanku',
