672285d36c8f77d573ef3e809e283b9848412d1b
tiararahmania05 <tiararahmania05@gmail.com>
2021-07-26 09:50:17 +0700

    Gemastik Uhuy

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/NelayanKu';
+$config['base_url'] = 'https://nelayanku-21.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
