2cf3f1ccc2783260f6fd418d686fe43e1fbcfddc
tiararahmania05 <tiararahmania05@gmail.com>
2021-07-26 11:11:08 +0700

    Gemastik Uhuy

--- application/controllers/Artikel.php
+++ application/controllers/Artikel.php
@@ -8,6 +8,34 @@ Class Artikel extends CI_Controller {
 		$this->load->view('Landing/artikel');
 	}
 
-	
+	public function artikel1()
+	{	
+		$this->load->view('Landing/artikel1');
+	}
+
+	public function artikel2()
+	{	
+		$this->load->view('Landing/artikel2');
+	}
+
+	public function artikel3()
+	{	
+		$this->load->view('Landing/artikel3');
+	}
+
+	public function artikel4()
+	{	
+		$this->load->view('Landing/artikel4');
+	}
+
+	public function artikel5()
+	{	
+		$this->load->view('Landing/artikel5');
+	}
+
+	public function artikel6()
+	{	
+		$this->load->view('Landing/artikel6');
+	}
 }
 ?>
