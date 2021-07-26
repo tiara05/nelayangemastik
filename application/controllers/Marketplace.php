ab2d5d25c9ae135585a97cc001f00ab423c49d0c
tiararahmania05 <tiararahmania05@gmail.com>
2021-07-26 10:39:34 +0700

    Gemastik Uhuy

--- application/controllers/Marketplace.php
+++ application/controllers/Marketplace.php
@@ -6,22 +6,22 @@ Class Marketplace extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('barang_admin_model');
+		$this->load->model('Barangadmin_model');
 	}
 
 	public function index()
 	{
 		$data['main_view'] = "Landing/marketplace";
 		$data['detail'] = "Landing/detail";
- 		$data['buku'] = $this->barang_admin_model->get_buku();
- 		$data['barang'] = $this->barang_admin_model->get_barang1();
- 		$data['barang2'] = $this->barang_admin_model->get_barang2();
- 		$data['barang3'] = $this->barang_admin_model->get_barang3();
- 		$data['barang4'] = $this->barang_admin_model->get_barang4();
- 		$data['barangg1'] = $this->barang_admin_model->get_barangg1();
- 		$data['barangg2'] = $this->barang_admin_model->get_barangg2();
- 		$data['barangg3'] = $this->barang_admin_model->get_barangg3();
- 		$data['barangg4'] = $this->barang_admin_model->get_barangg4();
+ 		$data['buku'] = $this->Barangadmin_model->get_buku();
+ 		$data['barang'] = $this->Barangadmin_model->get_barang1();
+ 		$data['barang2'] = $this->Barangadmin_model->get_barang2();
+ 		$data['barang3'] = $this->Barangadmin_model->get_barang3();
+ 		$data['barang4'] = $this->Barangadmin_model->get_barang4();
+ 		$data['barangg1'] = $this->Barangadmin_model->get_barangg1();
+ 		$data['barangg2'] = $this->Barangadmin_model->get_barangg2();
+ 		$data['barangg3'] = $this->Barangadmin_model->get_barangg3();
+ 		$data['barangg4'] = $this->Barangadmin_model->get_barangg4();
 		$this->load->view('Landing/marketplace_template', $data);
 	}
 
@@ -32,7 +32,7 @@ Class Marketplace extends CI_Controller {
 
 	public function tambah_keranjang($id)
 	{
-		$barang = $this->barang_admin_model->find($id);
+		$barang = $this->Barangadmin_model->find($id);
 
 		$data = array(
 			'id_barang'		=> $barang->id_barang,
